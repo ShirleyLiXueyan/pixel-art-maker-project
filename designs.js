fb2c62ec601aa573c4d63dac0ff2f84c82c7b2df
lixueyan <421079462@qq.com>
2018-03-12 22:41:04 +0800

    Add funtions

--- designs.js
+++ designs.js
@@ -1,10 +1,25 @@
 // Select color input
 // Select size input
-
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
+function makeGrid(e) {
+    let height = $("#inputHeight").val();
+    let width = $("#inputWidth").val();
+    $("#pixelCanvas").find().remove();
+    for (let x = 1; x <= Number(width); x++) {
+        $("#pixelCanvas").append("<tr></tr>");
+    }
+    for (let y = 1; y <= Number(height); y++) {
+        $("#pixelCanvas").find("tr").append("<td></td>");
+    }
+    e.preventDefault();
 }
+
+$("#submit").click(makeGrid);
+
+// When click cube, change the color to picked color
+$("#pixelCanvas").on("click", "td", function() {
+    let color = $("#colorPicker").val();
+    $(this).css("background-color", color);
+});
+
+
