5960325b7f8dd54bb860adecca2b9abf85daa5b7
lixueyan <421079462@qq.com>
2018-03-12 23:40:02 +0800

    Clean table every time submit

--- designs.js
+++ designs.js
@@ -1,25 +1,31 @@
-// Select color input
-// Select size input
-// When size is submitted by the user, call makeGrid()
+//Function for making grids
 function makeGrid(e) {
     let height = $("#inputHeight").val();
     let width = $("#inputWidth").val();
-    $("#pixelCanvas").find().remove();
-    for (let x = 1; x <= Number(width); x++) {
+    //Clean table
+    $("tr").remove();
+    //Loop height
+    for (let y = 1; y <= Number(height); y++) {
         $("#pixelCanvas").append("<tr></tr>");
     }
-    for (let y = 1; y <= Number(height); y++) {
+    //Loop width
+    for (let x = 1; x <= Number(width); x++) {
         $("#pixelCanvas").find("tr").append("<td></td>");
     }
     e.preventDefault();
 }
+// When size is submitted by the user, call makeGrid()
+$(document).ready(function() {
+    $("#submit").click(makeGrid);
+});
 
-$("#submit").click(makeGrid);
-
-// When click cube, change the color to picked color
+// When click cell, change the color to picked color
 $("#pixelCanvas").on("click", "td", function() {
-    let color = $("#colorPicker").val();
+    let color = $("#colorPicker").val();// Select color input
     $(this).css("background-color", color);
 });
+
+
+
 
 
